4788632682f76c97bb154a524dcec2b1caa98d76
Jessica Aweya <jessaweya@gmail.com>
2021-12-04 18:03:26 +0000

    fixing HTML and client files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,88 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Returning tweet element in HTML strucutre 
+
+
+const data = [
+    {
+        "user": {
+            "name": "Newton",
+            "avatars": "https://i.imgur.com/73hZDYK.png"
+            ,
+            "handle": "@SirIsaac"
+        },
+        "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+        },
+        "created_at": 1461116232227
+    },
+    {
+        "user": {
+            "name": "Descartes",
+            "avatars": "https://i.imgur.com/nlhLi3I.png",
+            "handle": "@rd"
+        },
+        "content": {
+            "text": "Je pense , donc je suis"
+        },
+        "created_at": 1461113959088
+    }
+]
+
+const renderTweets = function (tweets) {
+    $('.tweet-container').empty();
+
+    for (let tweet of tweets) {
+        const result = createTweetElement(tweet);
+        $('.tweet-container').prepend(result);
+    }
+};
+
+
 const createTweetElement = function (tweet) {
     let $tweet =
-        ` <article class=" article-header">
-            < header class="firsttweet">
-                <img class="${escape(tweet.user.avatars)}">
-                <h2 class = "username">${escape(tweet.user.name)} </h2>
-                <h3 class = "Userhandle">${escape(tweet.user.handle)}</h3>
+        ` <article id="article-header">
+            <header class="firsttweet">
+                <img src="${escape(tweet.user.avatars)}">
+                <span> class ="username">${escape(tweet.user.name)} </span>
+                <h3 class ="userhandle">${escape(tweet.user.handle)}</h3>
             </header>
+            <section>
             <p class="content">${escape(tweet.content.text)}</p>
+            
             <footer>
                 <span>
                     <i class="fa-solid fa-heart"></i>
                     <i class="fa-solid fa-retweet"></i>
                     <i class="fa-solid fa-flag"></i>
                 </span>
-                <p class="date">${(tweet.created_at).fromNow()}</p>
+                <p class="date">${(tweet.created_at)}</p>
             </footer>
-        </article>;`
+        </article>`
+    // console.log($tweet);
     return $tweet;
 };
 
-console.log($tweet);
+$(document).ready(function () {
+    renderTweets(data);
+});
+
+//renderTweets(data);
 
 
-const renderTweets = function (tweets) {
-    $('tweets-container').empty();
-    for (let tweet of tweets) {
-        const result = createTweetElement(tweet);
-        $('tweet-container').prepend(result);
-    }
-}
+// var post = function () {
+//     $(".tweet-form").submit(function (event) {
+//         event.preventDefault();
+//         if ('#tweet-text').val().length > 140 {
+//             $(".errors").slideDown("slow");
+//         } else if $('#tweet-text').val().length === 0) {
+//             $(".errors").slideDown("slow");
+//         } else {
+//             $.ajax({
+//                 url: "/tweets",
+//                 method: "POST",
+//                 data: $(this).seralize()
+//             })
+//         }
 
+//     }
